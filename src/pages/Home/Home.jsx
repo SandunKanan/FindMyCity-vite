7313426b7b90530cd67aa12ee5c582fc77f8ac1a
Sandun Kanangama <sandunkanangama@gmail.com>
2024-05-24 22:34:19 +0900

    adding remainder

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.scss';
+// import Cards from '../../components/Cards';
+import Hero from '../../components/Hero/Hero';
+// import Footer from '../../components/Footer';
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      {/* <Cards /> */}
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
