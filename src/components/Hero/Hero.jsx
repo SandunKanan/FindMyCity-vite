4e624f760a95d22f4b91f30089fab154492d8992
Sandun Kanangama <sandunkanangama@gmail.com>
2024-06-11 12:42:50 +0900

    Update to add image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,8 @@ import video from '../../assets/videos/video-2.mp4'
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src={video} autoPlay loop muted />
+      {/* <video src={video} autoPlay loop muted /> */}
+      <img src={video} alt="video" />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
